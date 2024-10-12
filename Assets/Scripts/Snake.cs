52303837f817daa5018af6f52e70ba135c758d91
AkuroP <thomas.kang2002@gmail.com>
2024-10-12 16:05:54 -0400

    [Add] Snake SpriteSheet + sprite movement

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -19,12 +19,38 @@ public class Snake : MonoBehaviour
     private float m_timeMovement;
 
     [Header("Corps")]
+    private SpriteRenderer m_snakeSprite;
     [SerializeField, Tooltip("Corps du snake")]
     private GameObject m_snakeBody;
 
     [SerializeField, Tooltip("Endroit de spawn du corps")]
-    private Transform m_snakeSpawnBody;
+    private Transform[] m_snakeSpawnBody;
 
+    [SerializeField]
+    private Sprite[] headSprites;
+
+    [SerializeField]
+    private Sprite[] bodySprites;
+
+    [SerializeField]
+    private Sprite m_headSprite;
+
+    [SerializeField]
+    private Sprite m_bodySprite;
+
+    public enum Movement
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    [SerializeField]
+    private Movement m_actualMovement;
+
+    [SerializeField]
+    private Movement m_previousMovement;
     [SerializeField, Tooltip("mort du snake")]
     private bool m_isDead;
 
@@ -35,6 +61,11 @@ public class Snake : MonoBehaviour
     {
         m_dir = transform.up;
         m_timeMovement = timeMovement;
+        m_headSprite = headSprites[0];
+        m_bodySprite = bodySprites[0];
+        m_actualMovement = Movement.UP;
+        m_previousMovement = Movement.UP;
+        m_snakeSprite = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -45,23 +76,87 @@ public class Snake : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             m_dir = new Vector3(-distanceMovement, 0, 0);
-            this.transform.rotation = Quaternion.Euler(0,0,90);
+            //this.transform.rotation = Quaternion.Euler(0,0,90);
+            
+            switch(m_previousMovement)
+            {
+                case Movement.UP:
+                    m_bodySprite = bodySprites[2];
+                    break;
+
+                case Movement.DOWN:
+                    m_bodySprite = bodySprites[3];
+                    break;
+            }
+            m_headSprite = headSprites[2];
+            m_actualMovement = Movement.LEFT;
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
             m_dir = new Vector3(distanceMovement, 0, 0);
-            this.transform.rotation = Quaternion.Euler(0, 0, -90);
+            //this.transform.rotation = Quaternion.Euler(0, 0, -90);
+
+            switch (m_previousMovement)
+            {
+                case Movement.UP:
+                    m_bodySprite = bodySprites[1];
+                    break;
+
+                case Movement.DOWN:
+                    m_bodySprite = bodySprites[4];
+                    break;
+            }
+
+            m_headSprite = headSprites[3];
+            m_actualMovement = Movement.RIGHT;
 
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             m_dir = new Vector3(0, distanceMovement, 0);
-            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //this.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            switch (m_previousMovement)
+            {
+                case Movement.LEFT:
+                    m_bodySprite = bodySprites[4];
+                    break;
+
+                case Movement.RIGHT:
+                    m_bodySprite = bodySprites[3];
+                    break;
+
+                case Movement.UP:
+                    m_bodySprite = bodySprites[0];
+                    break;
+            }
+
+            m_headSprite = headSprites[0];
+            m_actualMovement = Movement.UP;
+
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             m_dir = new Vector3(0, -distanceMovement, 0);
-            this.transform.rotation = Quaternion.Euler(0, 0, 180);
+            //this.transform.rotation = Quaternion.Euler(0, 0, 180);
+
+            switch (m_previousMovement)
+            {
+                case Movement.LEFT:
+                    m_bodySprite = bodySprites[1];
+                    break;
+
+                case Movement.RIGHT:
+                    m_bodySprite = bodySprites[2];
+                    break;
+
+                case Movement.DOWN:
+                    m_bodySprite = bodySprites[0];
+                    break;
+            }
+            m_headSprite = headSprites[1];
+            m_actualMovement = Movement.DOWN;
+
         }
 
         //mouvement du snake
@@ -72,8 +167,41 @@ public class Snake : MonoBehaviour
             this.transform.position += m_dir;
             m_timeMovement = timeMovement;
             //verifier la collision avant de spawn un nouveau body
-            Instantiate(m_snakeBody, m_snakeSpawnBody.position, this.transform.rotation);
+            GameObject body;
+            if (m_previousMovement == m_actualMovement) body = InstantiateLongBody();
+            else body = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.identity);
+            
+            body.GetComponent<SpriteRenderer>().sprite = m_bodySprite;
+            m_snakeSprite.sprite = m_headSprite;
+            m_previousMovement = m_actualMovement;
+
         }
+    }
+
+    private GameObject InstantiateLongBody()
+    {
+        GameObject b;
+        m_bodySprite = bodySprites[0];
+        switch(m_actualMovement)
+        {
+            case Movement.UP:
+                b = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.identity);
+                break;
+            case Movement.DOWN:
+                b = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.Euler(0,0,180));
+                break;
+            case Movement.LEFT:
+                b = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.Euler(0,0,90));
+                break;
+            case Movement.RIGHT:
+                b = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.Euler(0,0,-90));
+                break;
+
+            default:
+                b = Instantiate(m_snakeBody, m_snakeSpawnBody[(int)m_actualMovement].position, Quaternion.identity);
+            break;
+        }
+        return b;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
