6cf3d884a526ff2f7b885993c08f301657f97b30
Thoma-fr <thomas.raulin.pro@gmail.com>
2024-10-13 09:01:01 -0400

    1Dlobby

---
+++ Assets/lobby/DialogueBox.cs
@@ -0,0 +1,38 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField] List<string> _textlist = new List<string>();
+    [SerializeField] private TextMeshPro _textMeshPro;
+    [SerializeField] private float _textDuration = 2f;
+    [SerializeField] private GameObject _playeribjectref;
+    
+    void Start()
+    {
+        StartCoroutine(DisplayText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator DisplayText()
+    {
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < _textlist.Count; i++)
+        {
+            _textMeshPro.text = _textlist[i];
+            yield return new WaitForSeconds(_textDuration);
+            _playeribjectref.transform.DOShakeScale(.2f);
+            transform.DOShakeScale(.2f);
+        }
+        gameObject.SetActive(false);
+        //SceneManager.LoadScene("Lobby1D");
+    }
+}
